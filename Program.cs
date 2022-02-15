700cce8e3499eaebdcd6602ca09a9ac736f79615
vyshnavi1996 <77766043+vyshnavi1996@users.noreply.github.com>
2022-02-15 14:13:22 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,46 +1,7 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-using Auth0.AspNetCore.Authentication; // 👈 new code
-
-var builder = WebApplication.CreateBuilder(args);
-builder.Services
-    .AddAuth0WebAppAuthentication(options => {
-      options.Domain = builder.Configuration["Auth0:Domain"];
-      options.ClientId = builder.Configuration["Auth0:ClientId"];
-    });
-// 👆 new code
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-=======
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
 namespace Event_Management
->>>>>>> c361744cd1734c1ff96d614ac68102f056614386
-=======
-=======
->>>>>>> parent of c524215 (changes made for all the files)
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace classReminder
-<<<<<<< HEAD
->>>>>>> parent of c524215 (changes made for all the files)
-=======
->>>>>>> parent of c524215 (changes made for all the files)
 {
     public class Program
     {
