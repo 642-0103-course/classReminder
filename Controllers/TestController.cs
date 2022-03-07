9b5a8dc166e8ece59bf760566309bc2dace51515
Giridhar <giridhar196@gmail.com>
2022-03-06 21:05:57 -0600

    Updated test cases

---
+++ Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Event_Management.Models;
+using NUnit.Framework;
+
+namespace Event_Management.Controllers
+{
+    [TestFixture]
+    public class TestController
+    {
+        [Test]
+        public void GetReturnsProduct()
+        {
+            User user = new User();
+            user.Email = "";
+            user.Name = "";
+            user.Password = "";
+            Assert.AreNotEqual(10, 1);
+        }
+    }
+}
