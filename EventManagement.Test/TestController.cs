9cbf643b016161953b95f80fa48e1381cb40526f
Giridhar <giridhar196@gmail.com>
2022-03-17 12:29:45 -0500

    Updated unit test cases for the coverage

--- EventManagement.Test/TestController.cs
+++ EventManagement.Test/TestController.cs
@@ -1,74 +1,69 @@
-﻿
-using Event_Management.Controllers;
+﻿using Event_Management.Controllers;
 using Event_Management.Models;
 using Event_Management.MongodbContext;
 using Event_Management.Services;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Hosting.Internal;
-using MongoDB.Driver;
-using Moq;
 using NUnit.Framework;
+
 
 namespace EventManagement.Test
 {
     [TestFixture]
     public class TestController
     {
-        private EventsController eventsController;
-        private readonly Mock<EventService> eventService;
-        private readonly Mock<IWebHostEnvironment> webHostEnvironment;
-        private readonly Mock<IDatabaseSettings> databaseSettings;
-        private readonly Mock<IMongoClient> mockClient;
-        private readonly Mock<IMongoDatabase> mockDb;
+        public EventService ev;
+        public string ConnectionString = "mongodb+srv://admin:admin@cluster0.6vdu6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+        public string DatabaseName = "Events";
+        IDatabaseSettings settings;
+        EventsController eventsController;
+        public string id = "";
 
-        public TestController()
+        [SetUp]
+        public void Setup()
         {
-            //x.Verify();
-            //this.eventService = new EventService(x);
-            //webHostEnvironment = new IWebHostEnvironment();
-            databaseSettings = new Mock<IDatabaseSettings>();
-            databaseSettings.Setup(x => x.ConnectionString).Returns("mongodb+srv://admin:admin@cluster0.6vdu6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
-            databaseSettings.Setup(x => x.DatabaseName).Returns("Event");
-            eventService = new Mock<EventService>(databaseSettings.Object);
-            webHostEnvironment = new Mock<IWebHostEnvironment>();
-            eventsController = new EventsController(eventService.Object, webHostEnvironment.Object);
-            mockClient = new Mock<IMongoClient>();
-            mockDb = new Mock<IMongoDatabase>();
-
+            settings = new DatabaseSettings();
+            settings.ConnectionString = ConnectionString;
+            settings.DatabaseName = DatabaseName;
+            ev = new EventService(settings);
+            eventsController = new EventsController(ev, null);
         }
-        //[SetUp]
-        //public void Setup()
-        //{
-        //    var hostingEnvironment = Mock.Of<IWebHostEnvironment>(e => e.ApplicationName == "Class Reminder");
-           
-        //    eventsController = new EventsController(eventService, hostingEnvironment);
-        //}
+
+
 
         [Test]
-        public void GetReturnsProduct()
+        public void TestGet()
         {
-            eventsController.Create();
-            //User user = new User();
-            //user.Email = "";
-            //user.Name = "";
-            //user.Password = "";
-            //Assert.AreNotEqual(10, 1);
-        }
-        [Test]
-        public void test()
-        {
-            //var z = eventsController.Index();
-            // Assert.AreNotEqual(null, z);
-            EventModel eventModel = new EventModel()
+            var result = ev.Get();
+            if (result != null)
             {
-                Id = "1214",
-                EventName = "sample"
-            };
-            //mockDb.Setup(c => c.GetCollection<EventModel>("Event", null)).Returns(eventModel);
-            var c = eventsController.Edit("6192bbfe982af0dd0be02dca");
-            Assert.AreNotEqual(null, c);
+                id = result[0].Id;
+                Assert.AreNotEqual(null, result);
+            }
+            else
+            {
+                Assert.AreEqual(null, result);
+            }
+            
 
         }
+        [Test]
+        public void TestSearchList()
+        {
+            var x = ev.SearchList("giridhar196@gmail.com");
+            if (x == null)
+            {
+                Assert.AreNotEqual(null, ev.SearchList(id));
+            }
+            else
+            {
+                Assert.AreNotEqual(null, x);
+            }
+        }
+        [Test]
+        public void TestEditController()
+        {
+            var y = eventsController.Edit(id);
+            Assert.AreNotEqual(null, y);
+        }
+
     }
 }
